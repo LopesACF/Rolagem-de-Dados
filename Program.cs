7164b017920a9b6ae63a7a12a513715cf80a3c49
Altemilson Lopes <altemilsonlopes@gmail.com>
2023-04-29 17:47:55 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.Write("Quantas faces? ");
+    int faces = int.Parse(Console.ReadLine());
+
+    if (faces < 4) {
+      Console.WriteLine("Não é possível simular um dado com menos de 4 faces.");
+    } else {
+      Random random = new Random();
+      int resultado = random.Next(1, faces+1);
+      Console.WriteLine("\n--- Rolagem de Dado ---\n");
+      Console.WriteLine("Rolagem: " + resultado);
+    }
